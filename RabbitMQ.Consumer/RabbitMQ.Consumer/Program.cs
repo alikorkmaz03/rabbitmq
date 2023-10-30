50c863cbd4635345d1c58489d9686b4a296797a6
Ali Korkmaz <alikorkmaz03@gmail.com>
2023-10-31 01:00:05 +0300

    Added RabitMQ Consumer

---
+++ RabbitMQ.Consumer/RabbitMQ.Consumer/Program.cs
@@ -0,0 +1,23 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+//Create Connection
+ConnectionFactory factory = new();
+factory.Uri = new Uri("amqps://acggcrmh:zagmC6HPcGM0J9wO6DkhnPaI1z7N2Vwr@fish.rmq.cloudamqp.com/acggcrmh");
+//Activating the connection and opening a channel
+using IConnection connection = factory.CreateConnection();
+using IModel channel = connection.CreateModel();
+//Create Queue
+//(exclusive) The queue can be used by multiple connections.The queue must be declared with the exact same configuration as the publisher
+channel.QueueDeclare(queue: "example-queue", exclusive: false);
+//Read Message in Queue
+EventingBasicConsumer consumer = new(channel);
+channel.BasicConsume(queue: "example-queue",false,consumer);
+consumer.Received += (sender, e) =>
+{
+    //the place where the message arriving in the queue is processed
+    //e.Body.Span  or e.Body.ToArray()
+
+    Console.WriteLine(Encoding.UTF8.GetString(e.Body.Span));
+};
+Console.Read();
---
+++ RabbitMQ.Consumer/RabbitMQ.Consumer/Readme.md
@@ -0,0 +1,7 @@
+RabbitMQ Nugget Package for Consumer
+
+->dotnet add package RabbitMQ.Client --version 6.6.0
+- Create Connection
+- Activating the connection and opening a channel
+- Create Queue
+- Read Message in Queue
