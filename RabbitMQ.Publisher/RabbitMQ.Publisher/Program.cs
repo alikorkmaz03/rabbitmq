dad3e469620ad3feefd98545fdd76451559ac9b6
Ali Korkmaz <alikorkmaz03@gmail.com>
2023-10-31 00:34:20 +0300

    created RabbitMQ Publisher and send mesage queue to RabbitMQ

---
+++ RabbitMQ.Publisher/RabbitMQ.Publisher/Program.cs
@@ -0,0 +1,17 @@
+﻿using RabbitMQ.Client;
+using System.Text;
+//Create Connection
+ConnectionFactory factory = new();
+factory.Uri = new("amqps://acggcrmh:zagmC6HPcGM0J9wO6DkhnPaI1z7N2Vwr@fish.rmq.cloudamqp.com/acggcrmh");
+
+//Activating the connection and opening a channel
+using IConnection connection = factory.CreateConnection();
+using IModel channel = connection.CreateModel();
+
+//Create Queue
+channel.QueueDeclare(queue: "example-queue",exclusive:false);
+
+//Send Queue (!important :  RabbitMQ accepts the messages it sends to the queue in byte array type.We should convert byte array type)
+byte[] message = Encoding.UTF8.GetBytes("İlk RabbitMQ Mesajı");
+channel.BasicPublish(exchange: "", routingKey: "example-queue",body:message);//exchange default using direct exchange
+Console.Read();
---
+++ RabbitMQ.Publisher/RabbitMQ.Publisher/Readme.md
@@ -0,0 +1,6 @@
+RabbitMQ Nugget Package for Publisher
+->dotnet add package RabbitMQ.Client --version 6.6.0   
+- Create Connection
+- activating the connection and opening a channel
+- Create Queue
+- Send Queue
