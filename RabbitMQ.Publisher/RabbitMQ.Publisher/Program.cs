cbe6c72e18d332cea3421c7362496739260ae959
Ali Korkmaz <alikorkmaz03@gmail.com>
2023-11-06 22:40:48 +0300

    added message durability

--- RabbitMQ.Publisher/RabbitMQ.Publisher/Program.cs
+++ RabbitMQ.Publisher/RabbitMQ.Publisher/Program.cs
@@ -9,16 +9,21 @@ using IConnection connection = factory.CreateConnection();
 using IModel channel = connection.CreateModel();
 
 //Create Queue
-channel.QueueDeclare(queue: "example-queue",exclusive:false);
+//Kuyruklar içindeki mesajları kalıcı hale getirmek için durable özelliği true kullanılır.
+channel.QueueDeclare(queue: "example-queue",exclusive:false,durable:true);
 
 //Send Queue (!important :  RabbitMQ accepts the messages it sends to the queue in byte array type.We should convert byte array type)
 //byte[] message = Encoding.UTF8.GetBytes("İlk RabbitMQ Mesajı");
 //channel.BasicPublish(exchange: "", routingKey: "example-queue",body:message);//exchange default using direct exchange
 
+IBasicProperties properties = channel.CreateBasicProperties();
+properties.Persistent = true;
+
 for (int i = 0; i < 100; i++)
 {
     await Task.Delay(200);
-    byte[] message = Encoding.UTF8.GetBytes("İlk RabbitMQ Mesajı" + i);
-    channel.BasicPublish(exchange: "", routingKey: "example-queue",body:message);//exchange default using direct exchange
+    byte[] message = Encoding.UTF8.GetBytes("RabbitMQ Mesajı" + i);
+    //basicProperties:properties kalıcı ileti olucak fakat iletinin tam kayıpolmayacağını garanti etmez. outbox inbox design pattern kullanmak gerekebilir.
+    channel.BasicPublish(exchange: "", routingKey: "example-queue",body:message,basicProperties:properties);//exchange default using direct exchange
 }
 Console.Read();
