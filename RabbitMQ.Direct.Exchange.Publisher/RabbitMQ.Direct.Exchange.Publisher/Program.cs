649bf9b5e43a50a37db01ab58605d9b3669bd0b8
Ali Korkmaz <alikorkmaz03@gmail.com>
2023-11-06 23:50:46 +0300

    direct exchange  sample for Publisher

---
+++ RabbitMQ.Direct.Exchange.Publisher/RabbitMQ.Direct.Exchange.Publisher/Program.cs
@@ -0,0 +1,27 @@
+﻿using RabbitMQ.Client;
+using System.Text;
+//Create Connection
+ConnectionFactory factory = new();
+factory.Uri = new("amqps://acggcrmh:zagmC6HPcGM0J9wO6DkhnPaI1z7N2Vwr@fish.rmq.cloudamqp.com/acggcrmh");
+
+//Activating the connection and opening a channel
+using IConnection connection = factory.CreateConnection();
+using IModel channel = connection.CreateModel();
+
+channel.ExchangeDeclare(
+    exchange: "direct-exchange-example", 
+    type: ExchangeType.Direct);
+
+while (true)
+{
+    Console.Write("Mesaj : " );
+    string message = Console.ReadLine();
+    byte[] byteMsg = Encoding.UTF8.GetBytes(message);
+
+    channel.BasicPublish(
+        exchange: "direct-exchange-example",
+        routingKey:"direct-queue-example",
+        body:byteMsg);
+}
+
+Console.Read();
