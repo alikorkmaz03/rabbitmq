ab0e6678686be31f4c7d7b3113a83c055d7742d0
Ali Korkmaz <alikorkmaz03@gmail.com>
2023-11-07 01:39:23 +0300

    topic exchange for Publisher

---
+++ RabbitMQ.Topic.Exchange.Publisher/RabbitMQ.Topic.Exchange.Publisher/Program.cs
@@ -0,0 +1,31 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+//Create Connection
+ConnectionFactory factory = new();
+factory.Uri = new("amqps://acggcrmh:zagmC6HPcGM0J9wO6DkhnPaI1z7N2Vwr@fish.rmq.cloudamqp.com/acggcrmh");
+
+//Activating the connection and opening a channel
+using IConnection connection = factory.CreateConnection();
+using IModel channel = connection.CreateModel();
+
+channel.ExchangeDeclare(
+    exchange: "topic-exchange-example",
+    type: ExchangeType.Topic
+    );
+
+
+for (int i = 0; i < 100; i++)
+{
+    await Task.Delay(200);
+    byte[] message = Encoding.UTF8.GetBytes($"Gönderilen Mesaj : {i} ");
+    Console.WriteLine("Mesajın gönderileceği topic formatını belirtiniz : ");
+    string topic = Console.ReadLine();
+    channel.BasicPublish(
+        exchange: "topic-exchange-example",
+        routingKey:topic,
+        body:message
+        );
+}
+
+Console.Read();
