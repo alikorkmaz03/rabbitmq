7a239c2e7df74fe108590fe1969c6f26bb2444e9
ALİ KORKMAZ <alikorkmaz03@gmail.com>
2023-11-14 23:29:52 +0300

    header exchange for Consumer

---
+++ RabbitMQ.Header.Exchange.Consumer/Program.cs
@@ -0,0 +1,46 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+//Create Connection
+ConnectionFactory factory = new();
+factory.Uri = new("amqps://acggcrmh:zagmC6HPcGM0J9wO6DkhnPaI1z7N2Vwr@fish.rmq.cloudamqp.com/acggcrmh");
+
+//Activating the connection and opening a channel
+using IConnection connection = factory.CreateConnection();
+using IModel channel = connection.CreateModel();
+
+channel.ExchangeDeclare(
+    exchange: "header-exchange-example",
+    type: ExchangeType.Headers);
+
+
+
+Console.WriteLine("Lütfen header value'sunu giriniz: ");
+string? value = Console.ReadLine();
+
+
+string queueName = channel.QueueDeclare().QueueName;
+
+channel.QueueBind(
+    queue: queueName,
+    exchange: "header-exchange-example",
+    routingKey: string.Empty,
+    new Dictionary<string, object>
+    {
+        ["key"] = value
+    }
+);
+ 
+EventingBasicConsumer consumer = new(channel);
+channel.BasicConsume(
+    queue: queueName,
+    autoAck: true,
+    consumer: consumer);
+
+consumer.Received += (sender, e) =>
+{
+    string message = Encoding.UTF8.GetString(e.Body.Span);
+    Console.WriteLine(message);
+};
+
+Console.Read();
