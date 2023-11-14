34a9b2daad0e9e96b91a5d3a8d8c75fd78d3c5a8
ALİ KORKMAZ <alikorkmaz03@gmail.com>
2023-11-14 23:30:27 +0300

    header exchange for Publisher

---
+++ RabbitMQ.Header.Exchange.Publisher/Program.cs
@@ -0,0 +1,37 @@
+﻿using RabbitMQ.Client; 
+using System.Text;
+
+//Create Connection
+ConnectionFactory factory = new();
+factory.Uri = new("amqps://acggcrmh:zagmC6HPcGM0J9wO6DkhnPaI1z7N2Vwr@fish.rmq.cloudamqp.com/acggcrmh");
+
+//Activating the connection and opening a channel
+using IConnection connection = factory.CreateConnection();
+using IModel channel = connection.CreateModel();
+
+channel.ExchangeDeclare
+    (
+    exchange: "header-exchange-example",
+    type: ExchangeType.Headers
+    );
+for (int i = 0; i < 100; i++)
+{
+    await Task.Delay(200);
+    byte[] message = Encoding.UTF8.GetBytes($"Merhaba {i}");
+    Console.WriteLine("Lütfen header'ın value'sini girin ");
+    string? value = Console.ReadLine();
+
+    IBasicProperties basicProperties = channel.CreateBasicProperties();
+    basicProperties.Headers = new Dictionary<string, object>
+    {
+        ["key"] = value
+    };
+
+    channel.BasicPublish(
+        exchange: "header-exchange-example",
+        routingKey: string.Empty,
+        body: message,
+        basicProperties: basicProperties
+        );
+}
+Console.Read();
